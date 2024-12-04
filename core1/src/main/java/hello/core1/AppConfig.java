6167b5826689b427087ec040a83f5491c6eb3967
kjm541006 <kjm541113@gmail.com>
2024-12-04 16:34:42 +0900

    싱글톤

--- core1/src/main/java/hello/core1/AppConfig.java
+++ core1/src/main/java/hello/core1/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core1.member.MemberServiceImpl;
 import hello.core1.member.MemoryMemberRepository;
 import hello.core1.order.OrderService;
 import hello.core1.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberService(), discountPolicy());
     }
---
+++ core1/src/test/java/hello/core1/singleton/SingletonServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core1.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingletonServiceTest {
+
+    @Test
+    @DisplayName("싱글톤을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.logic();
+
+    }
+
+}
