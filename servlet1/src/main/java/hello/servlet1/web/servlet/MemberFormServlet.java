bc6e35a0973233aa686013721746e102850a5a72
kjm541006 <kjm541113@gmail.com>
2024-12-06 17:49:31 +0900

    servlet jsp mvc 패턴

---
+++ servlet1/src/main/java/hello/servlet1/web/servlet/MemberFormServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet1.web.servlet;
+
+import hello.servlet1.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+
+    }
+}
